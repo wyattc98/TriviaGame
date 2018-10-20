7f486e40c38a398a0eb2ebaa8f9520d57db6e222
Wyatt C <cochranwyatt@yahoo.com>
2018-10-20 16:13:34 -0700

    Due Date commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+
+$('document').ready(function () {
+  $('#questions').hide();
+  $('.answers').hide();
+
+  $("#startButton").click(function () {
+    $('#questions').show();
+    $(".answers").show();
+    $('#startButton').hide();
+    var score = 0;
+    //Timer function
+    var timeLeft = 60;
+    var gameTimer = setInterval(function () {
+      timeLeft--;
+      $("#countdown").text(timeLeft);
+      if (timeLeft <= 0) {
+        clearInterval(gameTimer);
+        alert("You got " + )
+      }
+    },1000)
+    var question1 = $("options1[]");
+    console.log(question1)
+  })
+});
